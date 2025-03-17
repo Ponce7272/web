1bd252a895ed798c7bf9453dab139635651c65e1
Ponce7272 <163880979+Ponce7272@users.noreply.github.com>
2025-03-16 20:10:23 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Función para mejorar el diseño del reproductor
+document.addEventListener("DOMContentLoaded", function() {
+    const audio = document.getElementById("audio");
+    audio.style.filter = "brightness(1) contrast(1.2)";
+});
